2e805470a6dcb43d3b76b50630fea29e8b3aa260
Bots.Business <git.sync@bots.business>
2022-08-20 13:21:03 +0000

    Bots.Business export for bot id: 709458

---
+++ commands/Broadcast/_broadc.js
@@ -0,0 +1,33 @@
+/*CMD
+  command: /broadc
+  help: 
+  need_reply: 
+  auto_retry_time: 
+  folder: Broadcast
+  answer: 
+  keyboard: 
+  aliases: 
+CMD*/
+
+var data = options.data
+var number = 1
+var msg =
+  "✅ *New task available*\n\n❓ We found " +
+  number +
+  " *new promotions* available for you today!\n\nUse /notification to disable this notification"
+Bot.sendMessageToChatWithId(data.split(" ")[no1], msg)
+Bot.sendMessageToChatWithId(data.split(" ")[no2], msg)
+Bot.sendMessageToChatWithId(data.split(" ")[no3], msg)
+Bot.sendMessageToChatWithId(data.split(" ")[no4], msg)
+Bot.sendMessageToChatWithId(data.split(" ")[no5], msg)
+var data = options.data
+var number = 1
+var msg =
+  "✅ *New task available*\n\n❓ We found " +
+  number +
+  " *new promotions* available for you today!\n\nUse /notification to disable this notification"
+Bot.sendMessageToChatWithId(data.split(" ")[no1], msg)
+Bot.sendMessageToChatWithId(data.split(" ")[no2], msg)
+Bot.sendMessageToChatWithId(data.split(" ")[no3], msg)
+Bot.sendMessageToChatWithId(data.split(" ")[no4], msg)
+Bot.sendMessageToChatWithId(data.split(" ")[no5], msg)

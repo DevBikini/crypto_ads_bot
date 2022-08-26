ad00351d27da5359f74a529965ad1f6d9a822a9a
Bots.Business <git.sync@bots.business>
2022-08-26 08:55:48 +0000

    Bots.Business export for bot id: 709458

--- commands/_.js
+++ commands/_.js
@@ -9,148 +9,150 @@
   aliases: 
 CMD*/
 
-var forward = User.getProperty("forward")
-if (forward == "forward-bot") {
-  if (!request.forward_from || request.forward_from.is_bot == "false") {
-    Bot.sendMessage("‼️* This is not forward from bot message*")
-    return
-  }
-  var bot_name = request.forward_from.username
-  Bot.sendMessage(
-    "*➕ Promotion\nCreation\n\nSend now this information*: `link`\n\n*🔎 Now send the link to the bot that you want to promote*.\n(All the traffic will be sent to that link)"
-  )
-  User.setProperty("forward", "", "string")
-  Bot.run({ command: "/bot#2", options: { name: bot_name } })
-  return
-}
-//view post
-var forward = User.getProperty("forward")
-if (forward == "forward-view") {
-  var cur = Bot.getProperty("admin_currency")
-  var cpc = Bot.getProperty("admin_cpc_view")
-  if (!request.forward_from_chat) {
-    var chat = request.chat.username
-    var message_id = request.message_id
-  } else {
-    var message_id = request.forward_from_message_id
-    var chat = request.forward_from_chat.username
-  }
-  Api.forwardMessage({
-    from_chat_id: user.telegramid,
-    message_id: request.message_id
-  })
-  Bot.sendMessage(
-    "*What is the most you want to pay per click?*\n\nMinimum Cost Per Click (CPC): *" +
-      cpc +
-      " " +
-      cur +
-      "*\n\n*➡️ Enter a value in " +
-      cur +
-      ":*"
-  )
-  User.setProperty("forward", "", "string")
-  Bot.run({
-    command: "/post#1",
-    options: {
-      name: "@" + chat,
-      link: "https://t.me/" + chat + "/" + message_id,
-      real_id: request.message_id
+if (request) {
+  var forward = User.getProperty("forward")
+  if (forward == "forward-bot") {
+    if (!request.forward_from || request.forward_from.is_bot == "false") {
+      Bot.sendMessage("‼️* This is not forward from bot message*")
+      return
     }
-  })
-  return
-}
-//second
-if (forward) {
-  if (!request.forward_from || request.forward_from.is_bot == "false") {
-    Bot.sendMessage("‼️* This is not forward from bot message*")
+    var bot_name = request.forward_from.username
+    Bot.sendMessage(
+      "*➕ Promotion\nCreation\n\nSend now this information*: `link`\n\n*🔎 Now send the link to the bot that you want to promote*.\n(All the traffic will be sent to that link)"
+    )
+    User.setProperty("forward", "", "string")
+    Bot.run({ command: "/bot#2", options: { name: bot_name } })
     return
   }
-  var forward_co = forward.split(" ")[0]
-  var dol = forward.split(" ")[1]
-  var ads = Bot.getProperty("all_in_ads")
-  var json = ads.list[forward_co]
-  var cur = Bot.getProperty("admin_currency")
-  var balance = Libs.ResourcesLib.anotherUserRes("balance", json.owner)
-  var payout = Libs.ResourcesLib.anotherUserRes("payout", json.owner)
-  if (!balance.value()) {
-    var balko = payout.value()
-    var green = payout.add(-json.cpc)
-  } else {
-    if (balance.value() < json.cpc) {
+  //view post
+  var forward = User.getProperty("forward")
+  if (forward == "forward-view") {
+    var cur = Bot.getProperty("admin_currency")
+    var cpc = Bot.getProperty("admin_cpc_view")
+    if (!request.forward_from_chat) {
+      var chat = request.chat.username
+      var message_id = request.message_id
+    } else {
+      var message_id = request.forward_from_message_id
+      var chat = request.forward_from_chat.username
+    }
+    Api.forwardMessage({
+      from_chat_id: user.telegramid,
+      message_id: request.message_id
+    })
+    Bot.sendMessage(
+      "*What is the most you want to pay per click?*\n\nMinimum Cost Per Click (CPC): *" +
+        cpc +
+        " " +
+        cur +
+        "*\n\n*➡️ Enter a value in " +
+        cur +
+        ":*"
+    )
+    User.setProperty("forward", "", "string")
+    Bot.run({
+      command: "/post#1",
+      options: {
+        name: "@" + chat,
+        link: "https://t.me/" + chat + "/" + message_id,
+        real_id: request.message_id
+      }
+    })
+    return
+  }
+  //second
+  if (forward) {
+    if (!request.forward_from || request.forward_from.is_bot == "false") {
+      Bot.sendMessage("‼️* This is not forward from bot message*")
+      return
+    }
+    var forward_co = forward.split(" ")[0]
+    var dol = forward.split(" ")[1]
+    var ads = Bot.getProperty("all_in_ads")
+    var json = ads.list[forward_co]
+    var cur = Bot.getProperty("admin_currency")
+    var balance = Libs.ResourcesLib.anotherUserRes("balance", json.owner)
+    var payout = Libs.ResourcesLib.anotherUserRes("payout", json.owner)
+    if (!balance.value()) {
       var balko = payout.value()
       var green = payout.add(-json.cpc)
     } else {
-      var green = balance.add(-json.cpc)
-      var balko = balance.value()
-    }
-  }
-  var sec = User.getProperty("User-" + json.ads)
-  var fee = json.cpc * 0.6
-  if (sec | (json.clicks > json.total) | (json.status == "Disabled 🚫")) {
-    //security ads
-    Bot.sendMessage("Sorry, That Task Is No Longer Valid. 😟")
-    return
-  }
-  if (json.cpc < balko) {
-    var botta = request.forward_from.username
-    if (botta == json.name) {
-      //complete task
-      var u_balance = Libs.ResourcesLib.userRes("payout")
-      u_balance.add(+fee)
-      //owner remove balance
-      green
-      Api.deleteMessage({
-        message_id: dol
-      })
-      Bot.sendMessage("✅* Task Completed*!\nYou earned: " + fee + " " + cur)
-      //referral
-      var referrer = Libs.ReferralLib.getAttractedBy()
-      if (referrer) {
-        var referrerRes = Libs.ResourcesLib.anotherUserRes(
-          "payout",
-          referrer.telegramid
-        )
-        var amount = fee * 0.15 // it is 15%
-        //referral earnings
-        referrerRes.add(+amount)
-      }
-      User.setProperty("User-" + json.ads, "done", "string")
-      Bot.runCommand("/bots")
-      if (json.clicks + 2 > json.total) {
-        var status = "Disabled 🚫"
+      if (balance.value() < json.cpc) {
+        var balko = payout.value()
+        var green = payout.add(-json.cpc)
       } else {
-        if (json.cpc > balko) {
-          var status = "⏸ *Paused*: budget reached or out of funds."
+        var green = balance.add(-json.cpc)
+        var balko = balance.value()
+      }
+    }
+    var sec = User.getProperty("User-" + json.ads)
+    var fee = json.cpc * 0.6
+    if (sec | (json.clicks > json.total) | (json.status == "Disabled 🚫")) {
+      //security ads
+      Bot.sendMessage("Sorry, That Task Is No Longer Valid. 😟")
+      return
+    }
+    if (json.cpc < balko) {
+      var botta = request.forward_from.username
+      if (botta == json.name) {
+        //complete task
+        var u_balance = Libs.ResourcesLib.userRes("payout")
+        u_balance.add(+fee)
+        //owner remove balance
+        green
+        Api.deleteMessage({
+          message_id: dol
+        })
+        Bot.sendMessage("✅* Task Completed*!\nYou earned: " + fee + " " + cur)
+        //referral
+        var referrer = Libs.ReferralLib.getAttractedBy()
+        if (referrer) {
+          var referrerRes = Libs.ResourcesLib.anotherUserRes(
+            "payout",
+            referrer.telegramid
+          )
+          var amount = fee * 0.15 // it is 15%
+          //referral earnings
+          referrerRes.add(+amount)
+        }
+        User.setProperty("User-" + json.ads, "done", "string")
+        Bot.runCommand("/bots")
+        if (json.clicks + 2 > json.total) {
+          var status = "Disabled 🚫"
         } else {
-          if (json.status == "Disabled 🚫") {
-            var status = "Disabled 🚫"
+          if (json.cpc > balko) {
+            var status = "⏸ *Paused*: budget reached or out of funds."
           } else {
-            var status = "Enabled ✅"
+            if (json.status == "Disabled 🚫") {
+              var status = "Disabled 🚫"
+            } else {
+              var status = "Enabled ✅"
+            }
           }
         }
+        var add = Bot.getProperty("all_in_ads", { list: {} })
+        add.list[forward_co] = {
+          ads: forward_co,
+          name: json.name,
+          link: json.link,
+          title: json.title,
+          description: json.description,
+          cpc: json.cpc,
+          clicks: json.clicks + 1,
+          budget: json.budget,
+          total: json.total,
+          status: status,
+          owner: json.owner,
+          promotion: json.promotion
+        }
+        Bot.setProperty("all_in_ads", add, "json")
+        User.setProperty("forward", "", "string")
+      } else {
+        Bot.sendMessage("‼️* This is not forward from bot message*")
       }
-      var add = Bot.getProperty("all_in_ads", { list: {} })
-      add.list[forward_co] = {
-        ads: forward_co,
-        name: json.name,
-        link: json.link,
-        title: json.title,
-        description: json.description,
-        cpc: json.cpc,
-        clicks: json.clicks + 1,
-        budget: json.budget,
-        total: json.total,
-        status: status,
-        owner: json.owner,
-        promotion: json.promotion
-      }
-      Bot.setProperty("all_in_ads", add, "json")
-      User.setProperty("forward", "", "string")
     } else {
-      Bot.sendMessage("‼️* This is not forward from bot message*")
+      Bot.sendMessage("Sorry, That Task Is No Longer Valid. 😟")
     }
-  } else {
-    Bot.sendMessage("Sorry, That Task Is No Longer Valid. 😟")
+    return
   }
-  return
 }
--- commands/🤖 Message Bots/_bot#6.js
+++ commands/🤖 Message Bots/_bot#6.js
@@ -43,80 +43,7 @@ if (message.includes("-") | !isNumeric(message) | (message < min_budgets)) {
   })
   return
 }
-if (message > balko) {
-  var ads = Generate(5)
-  var kol = TotalClick(min_budgets, min_budgets.length)
-  var number_click = kol * message
-  var node = number_click.toFixed(2)
-  if (node.includes(".")) {
-    var total_click = node.split(".")[0]
-  } else {
-    var total_click = number_click
-  }
-  Bot.sendInlineKeyboard(
-    [
-      [
-        { title: "✏️ Edit", command: "/edit bot " + ads },
-        { title: "Disable 🚫", command: "/disabled bot " + ads }
-      ]
-    ],
-    "*Campaign #" +
-      ads +
-      "* - Bot 🤖\n\n*Title*: " +
-      bot_title +
-      "\n*Description*: " +
-      bot_description +
-      "\n\n*Bot*: *@" +
-      bot_name +
-      "\nURL*: *" +
-      bot_url +
-      "\nStatus*: Enabled ✅\n\n*Daily budget*: " +
-      message +
-      " " +
-      cur +
-      "\n*CPC*: " +
-      bot_cpc +
-      " " +
-      cur +
-      "\n*Clicks*: " +
-      total_click +
-      " total / 0 today",
-    { disable_web_page_preview: true }
-  )
-  //set ads
-  var add = Bot.getProperty("all_in_ads", { list: {} })
-  add.list[ads] = {
-    ads: ads,
-    name: bot_name,
-    link: bot_url,
-    title: bot_title,
-    description: bot_description,
-    cpc: bot_cpc,
-    budget: message,
-    clicks: 0,
-    total: total_click,
-    status: "Enabled ✅",
-    owner: user.telegramid,
-    promotion: "Bot 🤖"
-  }
-  Bot.setProperty("all_in_ads", add, "json")
-  var myads = Libs.ResourcesLib.userRes("myads")
-  myads.add(1)
-  Api.sendMessage({
-    chat_id: "@CryptoAdAlert",
-    text:
-      "✅<b> New Ad Created</b>\n\nTask : 🤖 Message Bots\nCPC : <b>" +
-      options.cpc +
-      " " +
-      cur +
-      "</b>\nAvailable Submissions : <b>" +
-      total_click +
-      "</b>\n\nBot : @" +
-      bot.name +
-      "",
-    parse_mode: "html"
-  })
-} else {
+if (message > balko+0.002) {
   Bot.sendKeyboard(
     "➕ Deposit,🏠 Menu",
     "❌ You do not own enough " +
@@ -126,4 +53,77 @@ if (message > balko) {
       " " +
       cur
   )
+  return
 }
+var ads = Generate(5)
+var kol = TotalClick(min_budgets, min_budgets.length)
+var number_click = kol * message
+var node = number_click.toFixed(2)
+if (node.includes(".")) {
+  var total_click = node.split(".")[0]
+} else {
+  var total_click = number_click
+}
+Bot.sendInlineKeyboard(
+  [
+    [
+      { title: "✏️ Edit", command: "/edit bot " + ads },
+      { title: "Disable 🚫", command: "/disabled bot " + ads }
+    ]
+  ],
+  "*Campaign #" +
+    ads +
+    "* - Bot 🤖\n\n*Title*: " +
+    bot_title +
+    "\n*Description*: " +
+    bot_description +
+    "\n\n*Bot*: *@" +
+    bot_name +
+    "\nURL*: *" +
+    bot_url +
+    "\nStatus*: Enabled ✅\n\n*Daily budget*: " +
+    message +
+    " " +
+    cur +
+    "\n*CPC*: " +
+    bot_cpc +
+    " " +
+    cur +
+    "\n*Clicks*: " +
+    total_click +
+    " total / 0 today",
+  { disable_web_page_preview: true }
+)
+//set ads
+var add = Bot.getProperty("all_in_ads", { list: {} })
+add.list[ads] = {
+  ads: ads,
+  name: bot_name,
+  link: bot_url,
+  title: bot_title,
+  description: bot_description,
+  cpc: bot_cpc,
+  budget: message,
+  clicks: 0,
+  total: total_click,
+  status: "Enabled ✅",
+  owner: user.telegramid,
+  promotion: "Bot 🤖"
+}
+Bot.setProperty("all_in_ads", add, "json")
+var myads = Libs.ResourcesLib.userRes("myads")
+myads.add(1)
+Api.sendMessage({
+  chat_id: "@CryptoAdAlert",
+  text:
+    "✅<b> New Ad Created</b>\n\nTask : 🤖 Message Bots\nCPC : <b>" +
+    options.cpc +
+    " " +
+    cur +
+    "</b>\nAvailable Submissions : <b>" +
+    total_click +
+    "</b>\n\nBot : @" +
+    bot.name +
+    "",
+  parse_mode: "html"
+})

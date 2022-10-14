9189e6c986b12cac615e8d2497d85fcd5d9ccd62
Bots.Business <git.sync@bots.business>
2022-10-14 00:58:18 +0000

    Bots.Business export for bot id: 709458

--- commands/_admin_inspect.js
+++ commands/_admin_inspect.js
@@ -11,8 +11,21 @@ CMD*/
 
 var admin = Bot.getProperty("admin_admin")
 if (admin == user.telegramid) {
-  var id = params.split("&")[0]
-  var promotion = params.split("&")[1]
-  GettingAd(id, promotion)
+  var id = params
+  var index = Bot.getProperty("all_in_ads")
+  var json = index.list[id]
+  //Bot.inspect(json)
+  GettingAd(json.ads, json.promotion)
   return
 }
+
+var admin = Bot.getProperty("admin_admin")
+if (admin == user.telegramid) {
+  var id = params
+  var index = Bot.getProperty("all_in_ads")
+  var json = index.list[id]
+  //Bot.inspect(json)
+  GettingAd(json.ads, json.promotion)
+  return
+}
+

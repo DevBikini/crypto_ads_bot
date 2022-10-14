8b0a7ce3f6a59fb2e473b57ba1137c6088f14131
Bikini <codermanager80@gmail.com>
2022-10-14 09:02:21 +0800

    Update _admin_inspect.js

--- commands/_admin_inspect.js
+++ commands/_admin_inspect.js
@@ -19,13 +19,3 @@ if (admin == user.telegramid) {
   return
 }
 
-var admin = Bot.getProperty("admin_admin")
-if (admin == user.telegramid) {
-  var id = params
-  var index = Bot.getProperty("all_in_ads")
-  var json = index.list[id]
-  //Bot.inspect(json)
-  GettingAd(json.ads, json.promotion)
-  return
-}
-
